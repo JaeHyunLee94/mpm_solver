43f636efacd7d2901c58d001bbdcad0c2fcec9cf
JHLee Media <leejaehyun1223@gmail.com>
2022-09-15 23:44:25 +0900

    TODO: optimize

--- src/simulation/Engine.cpp
+++ src/simulation/Engine.cpp
@@ -25,6 +25,7 @@ void mpm::Engine::p2g(Scalar dt) {
     Vec3i base = (Xp - Vec3f(0.5, 0.5, 0.5)).cast<int>();
     Vec3f fx = Xp - base.cast<Scalar>();
     //TODO: cubic function
+    ////TODO: optimization candidate: so many constructor call?
     std::array<Vec3f, 3> w = {0.5 * Vec3f(pow(1.5 - fx[0], 2), pow(1.5 - fx[1], 2), pow(1.5 - fx[2], 2)),
                               Vec3f(0.75 - pow(fx[0] - 1, 2),
                                     0.75 - pow(fx[1] - 1, 2),
@@ -32,25 +33,28 @@ void mpm::Engine::p2g(Scalar dt) {
                               0.5 * Vec3f(pow(fx[0] - 0.5, 2), pow(fx[1] - 0.5, 2), pow(fx[2] - 0.5, 2))};
 
 //    Mat3f cauchy_stress = particle.getStress(particle);
+////TODO: optimization candidate: multiplication of matrix can be expensive.
     Mat3f cauchy_stress =  (10 * (pow(1./particle.m_Jp,7)-1))*Mat3f::Identity();
-    Mat3f stress =4*dt*cauchy_stress * particle.m_Jp*particle.m_V0 /(_grid.dx()*_grid.dx()); //TODO:no mass, volume
-    Mat3f affine = stress + particle.m_mass * particle.m_Cp; //TODO
+    Mat3f stress =4*dt*cauchy_stress * particle.m_Jp*particle.m_V0 /(_grid.dx()*_grid.dx()); ////TODO: optimization candidate: use inv_dx rather than dx
+    Mat3f affine = stress + particle.m_mass * particle.m_Cp;
 
     //Scatter the quantity
     for (int i = 0; i < 3; i++) {
       for (int j = 0; j < 3; ++j) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < 3; ++k) {
           Vec3i offset{i, j, k};
           Scalar weight = w[i][0] * w[j][1] * w[k][2];
           Vec3f dpos = (offset.cast<Scalar>() - fx) * _grid.dx();
           //i * _y_res * _z_res + j * _z_res + k
           Vec3i grid_index = base + offset;
+          ////TODO: optimization candidate: assign dimension out side of the loop
           unsigned int idx =
               grid_index[0] * _grid.getGridDimY() * _grid.getGridDimZ() + grid_index[1] * _grid.getGridDimZ()
                   + grid_index[2];
           Scalar mass_frag = weight * particle.m_mass;
           Vec3f momentum_frag = weight * (particle.m_mass * particle.m_vel + affine * dpos);
 
+          //TODO: optimization candidate: critical section?
 #pragma omp atomic
           _grid.m_mass[idx] += mass_frag;
 #pragma omp atomic
@@ -75,6 +79,9 @@ void mpm::Engine::updateGrid(Scalar dt) {
 
 #pragma omp parallel for
   for (int i = 0; i < _grid.getGridSize(); ++i) {
+    ////TODO: optimization candidate: should we iterate all? we can use continue;
+    ////TODO: optimization candidate: use signbit();
+
     if (_grid.m_mass[i] > 0) {
       _grid.m_vel[i] /= _grid.m_mass[i];
       _grid.m_vel[i] += dt * _gravity;
@@ -129,9 +136,9 @@ void mpm::Engine::g2p(Scalar dt) {
 
 
     //Scatter the quantity
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < 3; ++i) {
       for (int j = 0; j < 3; ++j) {
-        for (int k = 0; k < 3; k++) {
+        for (int k = 0; k < 3; ++k) {
           Vec3i offset{i, j, k};
           Scalar weight = w[i][0] * w[j][1] * w[k][2];
           Vec3f dpos = (offset.cast<Scalar>() - fx) * _grid.dx();
