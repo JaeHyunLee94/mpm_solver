faffdcc1f3eaf100503ddcba51923075668cbbce
JHLee Media <leejaehyun1223@gmail.com>
2022-09-22 20:09:07 +0900

    optimize

--- src/simulation/Engine.cpp
+++ src/simulation/Engine.cpp
@@ -15,42 +15,48 @@ void mpm::Engine::integrate(mpm::Scalar dt) {
   _currentFrame++;
 }
 
-void mpm::Engine::p2g(Scalar dt) {
+#define SQR(x) ((x)*(x))
 
-#pragma omp parallel for
-  for (int p = 0; p < m_sceneParticles.size(); ++p) {
-
-    auto &particle = m_sceneParticles[p];
+void mpm::Engine::p2g(Scalar dt)
+{
+  const Scalar _4_dt_invdx2 = 4.0f * dt * _grid.invdx() * _grid.invdx();
+#pragma omp parallel for schedule(dynamic)
+  for (int p = 0; p < m_sceneParticles.size(); ++p)
+  {
+    auto& particle = m_sceneParticles[p];
     Vec3f Xp = particle.m_pos * _grid.invdx();
-    Vec3i base = (Xp - Vec3f(0.5, 0.5, 0.5)).cast<int>();
+    Vec3i base = (Xp - Vec3f(0.5f, 0.5f, 0.5f)).cast<int>();
     Vec3f fx = Xp - base.cast<Scalar>();
     //TODO: cubic function
     ////TODO: optimization candidate: so many constructor call?
-    std::array<Vec3f, 3> w = {0.5 * Vec3f(pow(1.5 - fx[0], 2), pow(1.5 - fx[1], 2), pow(1.5 - fx[2], 2)),
-                              Vec3f(0.75 - pow(fx[0] - 1, 2),
-                                    0.75 - pow(fx[1] - 1, 2),
-                                    0.75 - pow(fx[2] - 1, 2)),
-                              0.5 * Vec3f(pow(fx[0] - 0.5, 2), pow(fx[1] - 0.5, 2), pow(fx[2] - 0.5, 2))};
+    Vec3f w[3] = { 0.5f * Vec3f(SQR(1.5f - fx[0]), SQR(1.5f - fx[1]), SQR(1.5f - fx[2])),
+                   Vec3f(0.75f - SQR(fx[0] - 1.0f),
+                         0.75f - SQR(fx[1] - 1.0f),
+                         0.75f - SQR(fx[2] - 1.0f)),
+                   0.5f * Vec3f(SQR(fx[0] - 0.5f), SQR(fx[1] - 0.5f), SQR(fx[2] - 0.5f)) };
 
 
-////TODO: optimization candidate: multiplication of matrix can be expensive.
+    ////TODO: optimization candidate: multiplication of matrix can be expensive.
     Mat3f cauchy_stress = particle.getStress(particle);//TODO: Std::bind
 
 
-    Mat3f stress =4*dt*cauchy_stress * particle.m_Jp*particle.m_V0 /(_grid.dx()*_grid.dx()); ////TODO: optimization candidate: use inv_dx rather than dx
+    Mat3f stress = cauchy_stress * (particle.m_Jp * particle.m_V0 * _4_dt_invdx2); ////TODO: optimization candidate: use inv_dx rather than dx
     Mat3f affine = stress + particle.m_mass * particle.m_Cp;
     //Scatter the quantity
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 3; ++j) {
-        for (int k = 0; k < 3; ++k) {
-          Vec3i offset{i, j, k};
+    for (int i = 0; i < 3; i++)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        for (int k = 0; k < 3; ++k)
+        {
+          Vec3i offset{ i, j, k };
           Scalar weight = w[i][0] * w[j][1] * w[k][2];
           Vec3f dpos = (offset.cast<Scalar>() - fx) * _grid.dx();
           //i * _y_res * _z_res + j * _z_res + k
           Vec3i grid_index = base + offset;
           ////TODO: optimization candidate: assign dimension out side of the loop
           unsigned int idx =
-              grid_index[0] * _grid.getGridDimY() * _grid.getGridDimZ() + grid_index[1] * _grid.getGridDimZ()
+              (grid_index[0] * _grid.getGridDimY() + grid_index[1]) * _grid.getGridDimZ()
                   + grid_index[2];
           Scalar mass_frag = weight * particle.m_mass;
           Vec3f momentum_frag = weight * (particle.m_mass * particle.m_vel + affine * dpos);
@@ -73,39 +79,42 @@ void mpm::Engine::p2g(Scalar dt) {
 
 }
 
-void mpm::Engine::updateGrid(Scalar dt) {
-  unsigned int x_dim = _grid.getGridDimX();
-  unsigned int y_dim = _grid.getGridDimY();
-  unsigned int z_dim = _grid.getGridDimZ();
+void mpm::Engine::updateGrid(Scalar dt)
+{
+  const unsigned int x_dim = _grid.getGridDimX();
+  const unsigned int y_dim = _grid.getGridDimY();
+  const unsigned int z_dim = _grid.getGridDimZ();
 
 #pragma omp parallel for
-  for (int i = 0; i < _grid.getGridSize(); ++i) {
+  for (int i = 0; i < _grid.getGridSize(); ++i)
+  {
     ////TODO: optimization candidate: should we iterate all? we can use continue;
     ////TODO: optimization candidate: use signbit();
 
-    if (_grid.m_mass[i] > 0) {
+    if (_grid.m_mass[i] > 0)
+    {
       _grid.m_vel[i] /= _grid.m_mass[i];
       _grid.m_vel[i] += dt * _gravity;
 
-      unsigned int xi = i/(y_dim * z_dim);
-      unsigned int yi = (i - xi * y_dim * z_dim)/z_dim;
+      unsigned int xi = i / (y_dim * z_dim);
+      unsigned int yi = (i - xi * y_dim * z_dim) / z_dim;
       unsigned int zi = i - xi * y_dim * z_dim - yi * z_dim;
-      if( xi<bound && _grid.m_vel[i][0] < 0){
+      if (xi < bound && _grid.m_vel[i][0] < 0) {
         _grid.m_vel[i][0] = 0;
       }
-      if( xi>x_dim-bound && _grid.m_vel[i][0] > 0){
+      else if (xi > x_dim - bound && _grid.m_vel[i][0] > 0) {
         _grid.m_vel[i][0] = 0;
       }
-      if( yi<bound && _grid.m_vel[i][1] < 0){
+      if (yi < bound && _grid.m_vel[i][1] < 0) {
         _grid.m_vel[i][1] = 0;
       }
-      if( yi>y_dim-bound && _grid.m_vel[i][1] > 0){
+      else if (yi > y_dim - bound && _grid.m_vel[i][1] > 0) {
         _grid.m_vel[i][1] = 0;
       }
-      if( zi<bound && _grid.m_vel[i][2] < 0){
+      if (zi < bound && _grid.m_vel[i][2] < 0) {
         _grid.m_vel[i][2] = 0;
       }
-      if( zi>z_dim-bound && _grid.m_vel[i][2] > 0){
+      else if (zi > z_dim - bound && _grid.m_vel[i][2] > 0) {
         _grid.m_vel[i][2] = 0;
       }
 
@@ -119,21 +128,22 @@ void mpm::Engine::updateGrid(Scalar dt) {
 
 }
 
-void mpm::Engine::g2p(Scalar dt) {
-
-
+void mpm::Engine::g2p(Scalar dt)
+{
+  const Scalar _4_invdx2 = 4.0f * _grid.invdx() * _grid.invdx();
 #pragma omp parallel for
-  for (int p = 0; p < m_sceneParticles.size(); ++p) {
-    auto &particle = m_sceneParticles[p];
-    Vec3f Xp =particle.m_pos * _grid.invdx();
-    Vec3i base = (Xp - Vec3f(0.5, 0.5, 0.5)).cast<int>();
+  for (int p = 0; p < m_sceneParticles.size(); ++p)
+  {
+    auto& particle = m_sceneParticles[p];
+    Vec3f Xp = particle.m_pos * _grid.invdx();
+    Vec3i base = (Xp - Vec3f(0.5f, 0.5f, 0.5f)).cast<int>();
     Vec3f fx = Xp - base.cast<Scalar>();
     //TODO: cubic function
-    std::array<Vec3f, 3> w = {0.5 * Vec3f(pow(1.5 - fx[0], 2), pow(1.5 - fx[1], 2), pow(1.5 - fx[2], 2)),
-                              Vec3f(0.75 - pow(fx[0] - 1, 2),
-                                    0.75 - pow(fx[1] - 1, 2),
-                                    0.75 - pow(fx[2] - 1, 2)),
-                              0.5 * Vec3f(pow(fx[0] - 0.5, 2), pow(fx[1] - 0.5, 2), pow(fx[2] - 0.5, 2))};
+    Vec3f w[3] = { 0.5f * Vec3f(SQR(1.5f - fx[0]), SQR(1.5f - fx[1]), SQR(1.5f - fx[2])),
+                   Vec3f(0.75f - SQR(fx[0] - 1),
+                         0.75f - SQR(fx[1] - 1),
+                         0.75f - SQR(fx[2] - 1)),
+                   0.5f * Vec3f(SQR(fx[0] - 0.5f), SQR(fx[1] - 0.5f), SQR(fx[2] - 0.5f)) };
 
     Vec3f new_v = Vec3f::Zero();
     Mat3f new_C = Mat3f::Zero();
@@ -141,17 +151,20 @@ void mpm::Engine::g2p(Scalar dt) {
 
 
     //Scatter the quantity
-    for (int i = 0; i < 3; ++i) {
-      for (int j = 0; j < 3; ++j) {
-        for (int k = 0; k < 3; ++k) {
-          Vec3i offset{i, j, k};
+    for (int i = 0; i < 3; ++i)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        for (int k = 0; k < 3; ++k)
+        {
+          Vec3i offset{ i, j, k };
           Scalar weight = w[i][0] * w[j][1] * w[k][2];
           Vec3f dpos = (offset.cast<Scalar>() - fx) * _grid.dx();
           //i * _y_res * _z_res + j * _z_res + k
           Vec3i grid_index = base + offset;
-          unsigned int idx = grid_index[0] * _grid.getGridDimY() * _grid.getGridDimZ() + grid_index[1] * _grid.getGridDimZ()+ grid_index[2];
+          unsigned int idx = (grid_index[0] * _grid.getGridDimY() + grid_index[1]) * _grid.getGridDimZ() + grid_index[2];
           new_v += weight * _grid.m_vel[idx];
-          new_C += 4*weight*_grid.m_vel[idx]*dpos.transpose()/(_grid.dx()*_grid.dx());
+          new_C += (weight * _4_invdx2) * _grid.m_vel[idx] * dpos.transpose();
 
         }
       }
@@ -160,9 +173,9 @@ void mpm::Engine::g2p(Scalar dt) {
     particle.m_vel = new_v;
     particle.m_Cp = new_C;
 
-    particle.m_pos +=dt*particle.m_vel;
+    particle.m_pos += dt * particle.m_vel;
 
-    particle.project(particle,dt);
+    particle.project(particle, dt);
 
 
   }
@@ -171,7 +184,7 @@ void mpm::Engine::g2p(Scalar dt) {
 
 }
 
-void mpm::Engine::addParticles(Particles &particles) {
+void mpm::Engine::addParticles(Particles& particles) {
   //TODO: const
   if (!_isCreated) {
     fmt::print("Engine not created yet\n");
@@ -203,14 +216,14 @@ void mpm::Engine::setGravity(Vec3f gravity) {
 void mpm::Engine::init() {
   _grid.resetGrid();
 }
-float *mpm::Engine::getGravityFloatPtr() {
+float* mpm::Engine::getGravityFloatPtr() {
   return _gravity.data();
 }
-void mpm::Engine::reset(Particles& particle,EngineConfig engine_config) {
+void mpm::Engine::reset(Particles& particle, EngineConfig engine_config) {
 
-  _engineConfig=engine_config;
+  _engineConfig = engine_config;
   deleteAllParticle();
-  setGravity(Vec3f(0,0,0));
+  setGravity(Vec3f(0, 0, 0));
   addParticles(particle);
 
 }
@@ -219,9 +232,9 @@ void mpm::Engine::deleteAllParticle() {
 
 }
 void mpm::Engine::setEngineConfig(EngineConfig engine_config) {
-    _engineConfig=engine_config;
+  _engineConfig = engine_config;
 }
-void mpm::Engine::integrateWithProfile(mpm::Scalar dt, Profiler &profiler) {
+void mpm::Engine::integrateWithProfile(mpm::Scalar dt, Profiler& profiler) {
 
   profiler.start("init");
   init();
--- src/simulation/MaterialModel.h
+++ src/simulation/MaterialModel.h
@@ -19,16 +19,17 @@ class MaterialModel {
      * TODO: Implement the weakly compressible model
      */
 //      printf("D\n");
-    Scalar pressure = (10 * (pow(1.f / p.m_Jp, 7) - 1));
+    Scalar m_Jp_3 = p.m_Jp * p.m_Jp * p.m_Jp;
+    Scalar pressure = (10.0f * (1.0f / (m_Jp_3 * m_Jp_3 * p.m_Jp) - 1));
 
     return pressure * Mat3f::Identity();
 
   }
   static Mat3f getStressCorotatedJelly(Particle &p) {
-    Scalar E = 1000; //Young's modulus
-    Scalar nu = 0.2;  //# Poisson's ratio
-    Scalar mu_0 = E / (2 * (1 + nu));
-    Scalar lambda_0 = E * nu / ((1 + nu) * (1 - 2 * nu)); // # Lame parameters
+    constexpr Scalar E = 1000; //Young's modulus
+    constexpr Scalar nu = 0.2;  //# Poisson's ratio
+    constexpr Scalar mu_0 = E / (2 * (1 + nu));
+    constexpr Scalar lambda_0 = E * nu / ((1 + nu) * (1 - 2 * nu)); // # Lame parameters
 
     Eigen::JacobiSVD<mpm::Mat3f> svd(p.m_F, Eigen::ComputeFullU | Eigen::ComputeFullV);
     Mat3f U = svd.matrixU();
@@ -38,8 +39,8 @@ class MaterialModel {
     Scalar inv_J = 1.f/J;
     p.m_Jp = J;
     Mat3f cauchy_stress =
-    inv_J *2 * mu_0 * (p.m_F - R) * p.m_F.transpose()  + lambda_0 * (J - 1) * Mat3f::Identity();
-    return -cauchy_stress;
+        (-inv_J * 2 * mu_0) * (p.m_F - R) * p.m_F.transpose() + (-lambda_0 * (J - 1)) * Mat3f::Identity();
+    return cauchy_stress;
   }
   static Mat3f getNeoHookeanStress(Particle &p) {
     return mpm::Mat3f();
