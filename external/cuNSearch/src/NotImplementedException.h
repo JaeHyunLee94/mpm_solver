d7b9cb937091e678d3c73d1d7acf5a98fec5f249
JHLee Media <leejaehyun1223@gmail.com>
2022-12-30 15:48:34 +0900

    cuNsearch

---
+++ external/cuNSearch/src/NotImplementedException.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+#include <iostream>
+
+class NotImplementedException : std::runtime_error
+{
+public:
+	NotImplementedException()
+		: std::runtime_error("Not implemented.")
+	{
+
+	}
+
+	NotImplementedException(std::string str)
+		: std::runtime_error("Not implemented: " + str)
+	{
+		std::cout << "Not implemented: " << str << std::endl;
+	}
+};
